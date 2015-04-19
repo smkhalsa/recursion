3f2d32b06aa28807ef782ea0bfe1332870f7121f
Sat Mandir Khalsa <smkhalsa@gmail.com>
2015-04-19 12:10:06 -0700

    stringifyJSON working except for Objects

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,26 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (typeof obj === 'undefined' || typeof obj === 'function') {
+    return '{}';
+  } else if (typeof obj === 'boolean' || typeof obj === 'number') {
+    return obj;
+  } else if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  } else if (typeof obj === 'object') {
+    if (Array.isArray(obj)) {
+      // if object is array
+      var results = [];
+      for (var i=0; i<obj.length; i++) {
+        results[i] = stringifyJSON(obj[i]);
+      }
+      return '[' + results.join(', ') + ']';
+    } else {
+      // if object is non-array object
+      var newObj = {};
+      for (var key in obj) {
+        newObj[key] = stringifyJSON(obj[key]);
+      }
+    }
+  }
 };
