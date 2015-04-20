3d825670691d755ba6d31a316fd800e5835aa4d2
Sat Mandir Khalsa <smkhalsa@gmail.com>
2015-04-19 18:56:06 -0700

    Completed, but not passing test

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,25 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+
+window.onload = function() {
+
+  var node = document.body;
+  var results = [];
+
+  var getElementsByClassName = function(className){
+    // your code here
+    if (node.classList.contains(className)) {
+      results.push(node);
+    }
+    if (node.hasChildNodes()) {
+      for (var i=0; i<node.children.length; i++) {
+        node = node.children[i];
+        getElementsByClassName(className);
+      }
+    }
+    return results;
+  };
+  console.log(getElementsByClassName("mocha"));
+  console.log(document.getElementsByClassName("mocha"));
 };
